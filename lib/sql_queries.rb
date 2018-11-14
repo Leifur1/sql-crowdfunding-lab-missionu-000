ab9da9d0d32b562b671d9bd67bec11ca727fc9e4
Leifur Sigurdsson <leifurerik@gmail.com>
2018-11-14 19:33:50 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,5 +1,5 @@
 # Write your sql queries in this file in the appropriate method like the example below:
-#
+# 
 # def select_category_from_projects
 # "SELECT category FROM projects;"
 # end
@@ -7,11 +7,11 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts
-  "SELECT a.title, SUM(amount) FROM projects a JOIN pledges b ON a.id = b.project_id GROUP BY a.title ORDER BY a.title ASC;"
+  "SELECT projects.title, SUM(pledges.amount) FROM projects JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title;"
 end
 
-def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-  "SELECT users.name, users.age, SUM(pledges.amount) FROM users JOIN pledges ON users.id = pledges.user_id GROUP BY users.name ORDER BY users.name;"
+def selects_the_user_name_age_and_pledge_amount_for_all_pledges
+  "SELECT users.name, users.age, SUM(pledges.amount) FROM users JOIN pledges ON users.id = pledges.user_id GROUP BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
